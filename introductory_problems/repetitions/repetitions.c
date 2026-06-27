858a6382e20c1803cc0aec17176793fe58791a9b
Samir Parikh <siparikh@gmail.com>
2026-06-27 16:48:59 -0400

    add repetitions

---
+++ introductory_problems/repetitions/repetitions.c
@@ -0,0 +1,41 @@
+// Repetitions
+// https://cses.fi/problemset/
+//
+// Read from stdin, write the answer to stdout.
+
+#include <stdio.h>
+#include <string.h>
+
+enum { ARRAY_SIZE = 100 };
+
+// Reads a line into x, strips the trailing newline.
+// Returns the length of the string, or -1 on EOF/error.
+int read_input(char *x, int size) {
+    if (fgets(x, size, stdin) == NULL) {
+        return -1;
+    }
+
+    size_t len = strlen(x);
+
+    if (len > 0 && x[len - 1] == '\n') {
+        x[len - 1] = '\0';
+        len--;
+    }
+
+    return (int)len;
+}
+
+int main(void) {
+    char x[ARRAY_SIZE];
+
+    int len = read_input(x, ARRAY_SIZE);
+    if (len < 0) {
+        return 0;
+    }
+
+    printf("x[0] = %c\n", x[0]);
+    printf("x[1] = %c\n", x[1]);
+    printf("x[2] = %c\n", x[2]);
+
+    return 0;
+}
