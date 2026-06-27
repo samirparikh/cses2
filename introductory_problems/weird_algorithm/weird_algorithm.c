4d305b57928ba0dcdb26dfe0c1de7a374b2d318b
Samir Parikh <siparikh@gmail.com>
2026-06-27 09:18:35 -0400

    fix spacing in weird algorithm

--- introductory_problems/weird_algorithm/weird_algorithm.c
+++ introductory_problems/weird_algorithm/weird_algorithm.c
@@ -15,16 +15,15 @@ int main(void) {
     long long n;
     scanf("%lld", &n);
 
-    printf("%lld", n);
+    printf("%lld ", n);
     while (n != 1) {
         if (n % 2 == 0) {
             n /= 2;
         } else {
             n = 3 * n + 1;
         }
-        printf(" %lld", n);
+        printf("%lld ", n);
     }
-    printf("\n");
 
     return 0;
 }
