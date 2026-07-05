d0ad478a81ed4e7907ad101a1e3072f65c812f48
Samir Parikh <siparikh@gmail.com>
2026-07-05 13:35:17 -0400

    get increasing array to work

--- introductory_problems/increasing_array/increasing_array.c
+++ introductory_problems/increasing_array/increasing_array.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Increasing Array
-// https://cses.fi/problemset/
-//
-// Read from stdin, write the answer to stdout.
+int main(void)
+{
+    int n;
+    long long moves = 0;
 
-int main(void) {
+    if (scanf("%d", &n) != 1) {
+        fprintf(stderr, "Invalid input\n");
+        return 1;
+    }
+
+    long long x[n];
+
+    for (int i = 0; i < n; i++) {
+        if (scanf("%lld", &x[i]) != 1) {
+            fprintf(stderr, "Invalide input\n");
+            return 1;
+        }
+    }
+
+    for (int i = 1; i < n; i++) {
+        if (x[i] < x[i-1]) {
+            moves += x[i-1] - x[i];
+            x[i] = x[i-1];
+        }
+    }
+
+    printf("%lld", moves);
 
     return 0;
 }
