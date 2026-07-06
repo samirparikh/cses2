cc53d3c314071c51494c7bd00bf5d9ca7e0ada67
Samir Parikh <siparikh@gmail.com>
2026-07-06 13:38:19 -0400

    get apartments solution to work locally

--- sorting_and_searching/apartments/apartments.c
+++ sorting_and_searching/apartments/apartments.c
@@ -43,15 +43,15 @@ int main(void) {
     qsort(desired_size, num_applicants, sizeof(desired_size[0]), compare_long_longs);
     qsort(apartment_size, num_apartments, sizeof(apartment_size[0]), compare_long_longs);
 
-    for (int i = 0; i < num_applicants; i++) {
-        printf("%lld ", desired_size[i]);
-    }
-    printf("\n");
-    
-    for (int i = 0; i < num_apartments; i++) {
-        printf("%lld ", apartment_size[i]);
-    }
-    printf("\n");
+    // for (int i = 0; i < num_applicants; i++) {
+    //     printf("%lld ", desired_size[i]);
+    // }
+    // printf("\n");
+    // 
+    // for (int i = 0; i < num_apartments; i++) {
+    //     printf("%lld ", apartment_size[i]);
+    // }
+    // printf("\n");
 
     // apartment matching loop
     int applicant_pointer = 0;
@@ -59,34 +59,54 @@ int main(void) {
     int matches = 0;
 
     while (applicant_pointer < num_applicants && apartment_pointer < num_apartments) {
-        long long current_applicant = desired_size[applicant_pointer];
-        long long current_apartment = apartment_size[apartment_pointer];
-        printf("comparing desired size of %lld with actual size of %lld",
-            current_applicant, current_apartment);
+        long long desired = desired_size[applicant_pointer];
+        long long actual  = apartment_size[apartment_pointer];
+        // printf("---------------------\n");
+        // printf("comparing desired size of %lld with actual size of %lld\n", desired, actual);
 
         // actual size is within tolerance
-        if (llabs(current_applicant - current_apartment) <= max_difference) {
-            printf("found apartment within tolerance\n");
+        if (llabs(desired - actual) <= max_difference) {
+            // printf("found apartment within tolerance\n");
             // increment matches
+            matches++;
             // move pointers
-
+            applicant_pointer++;
+            apartment_pointer++;
         }
         
-
         // actual size is below tolerance
-        else if (current_apartment < current_applicant - max_difference) {
-            printf("current apartment size is below tolerance\n");
+        else if (actual < desired - max_difference) {
+            // printf("current apartment size is below tolerance\n");
+            // if next apartment size is less than or equal to desired size + tolerance,
+            // increment only the apartment pointer, and go back to beginning of loop to assess it
+            // (this may be an area to optimize)
+            if (apartment_size[apartment_pointer+1] <= desired + max_difference) {
+                // printf("evaluating next apartment\n");
+                apartment_pointer++;
+            }
+
+            // otherwise, no apartments available for this applicant
+            // increment both pointers only
+            else {
+                // printf("no apartment found for this candidate\n");
+                applicant_pointer++;
+                apartment_pointer++;
+            }
 
         }
        
-
-
         // actual size is above tolerance
         else {
-            printf("current apartment size to above tolerance\n");
+            // printf("current apartment size is above tolerance\n");
+            // printf("no apartment found for this candidate\n");
+            // there is no suitable apartment for this candidate
+            // increment candidate pointer only
+            applicant_pointer++;
         }
     }
 
+    // printf("matches found: ");
+    printf("%d\n", matches);
 
     return 0;
 }
