f7d05377f1d3fda8152c60b011deadafa1b3a510
Samir Parikh <siparikh@gmail.com>
2026-07-06 19:09:36 -0400

    get ferris wheel working

--- sorting_and_searching/ferris_wheel/ferris_wheel.c
+++ sorting_and_searching/ferris_wheel/ferris_wheel.c
@@ -28,9 +28,49 @@ int main(void) {
         }
     }
 
+    // reverse sort array
     qsort(child_weight, num_children, sizeof(child_weight[1]), compare_long_longs);
 
-    for (int i = 0; i < num_children; i++) printf("%lld ", child_weight[i]); printf("\n");
+    // for (int i = 0; i < num_children; i++) printf("%lld ", child_weight[i]); printf("\n");
+
+    // initialize pointers
+    int heavy = 0;
+    int light = num_children - 1;
+
+    int gondolas = 0;
+
+    while (heavy <= light) {
+        // if child weight equals max weight, put one child in gondola and move to the next
+        // heaviest child by incrementing heavy pointer
+        // printf("-------------------\n");
+        // printf("top of loop: heavy = %d, light = %d\n", heavy, light);
+        // printf("comparing heavy (%lld) with light (%lld)\n", child_weight[heavy], child_weight[light]);
+        if (child_weight[heavy] == max_weight) {
+            // printf("heavy child equals max weight of %lld\n", max_weight);
+            gondolas++;
+            // printf("putting heavy child into gondola %d\n", gondolas);
+            heavy++;
+        }
+
+        // else, check to see if heavy child can be paired with light child
+        else if (child_weight[heavy] + child_weight[light] <= max_weight) {
+            // put both children in gondola and increment pointers
+            gondolas++;
+            // printf("able to put heavy and light children into gondola %d\n", gondolas);
+            heavy++;
+            light--;
+        }
+
+        // otherwise, put the less than max weight heavy child in gondola by themselves
+        else {
+            gondolas++;
+            // printf("less than max weight heavy child goes into gondola %d\n", gondolas);
+            heavy++;
+        }
+        // printf("bottom of loop: heavy = %d, light = %d\n", heavy, light);
+    }
+
+    printf("%d\n", gondolas);
 
     return 0;
 }
