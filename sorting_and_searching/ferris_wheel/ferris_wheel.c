ef1a73c5d4ab5f858fedaa157f27b342413c1100
Samir Parikh <siparikh@gmail.com>
2026-07-06 19:11:00 -0400

    clean up code

--- sorting_and_searching/ferris_wheel/ferris_wheel.c
+++ sorting_and_searching/ferris_wheel/ferris_wheel.c
@@ -31,8 +31,6 @@ int main(void) {
     // reverse sort array
     qsort(child_weight, num_children, sizeof(child_weight[1]), compare_long_longs);
 
-    // for (int i = 0; i < num_children; i++) printf("%lld ", child_weight[i]); printf("\n");
-
     // initialize pointers
     int heavy = 0;
     int light = num_children - 1;
@@ -42,13 +40,8 @@ int main(void) {
     while (heavy <= light) {
         // if child weight equals max weight, put one child in gondola and move to the next
         // heaviest child by incrementing heavy pointer
-        // printf("-------------------\n");
-        // printf("top of loop: heavy = %d, light = %d\n", heavy, light);
-        // printf("comparing heavy (%lld) with light (%lld)\n", child_weight[heavy], child_weight[light]);
         if (child_weight[heavy] == max_weight) {
-            // printf("heavy child equals max weight of %lld\n", max_weight);
             gondolas++;
-            // printf("putting heavy child into gondola %d\n", gondolas);
             heavy++;
         }
 
@@ -56,7 +49,6 @@ int main(void) {
         else if (child_weight[heavy] + child_weight[light] <= max_weight) {
             // put both children in gondola and increment pointers
             gondolas++;
-            // printf("able to put heavy and light children into gondola %d\n", gondolas);
             heavy++;
             light--;
         }
@@ -64,10 +56,8 @@ int main(void) {
         // otherwise, put the less than max weight heavy child in gondola by themselves
         else {
             gondolas++;
-            // printf("less than max weight heavy child goes into gondola %d\n", gondolas);
             heavy++;
         }
-        // printf("bottom of loop: heavy = %d, light = %d\n", heavy, light);
     }
 
     printf("%d\n", gondolas);
