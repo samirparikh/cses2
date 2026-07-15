fc48d18846df2caa43cc3d314bbfeffa56ebd469
Samir Parikh <siparikh@gmail.com>
2026-07-14 20:24:50 -0400

    update solution prior to submission

--- dmoj/snowflakes/snowflakes.c
+++ dmoj/snowflakes/snowflakes.c
@@ -1,10 +1,16 @@
 // dmoj: Snowflakes
 
 #include <stdio.h>
+#include <stdlib.h>
 #define SNOWFLAKE_SIDES      6
 #define SIZE            100000
 
-int hash(int snowflake[]) {
+typedef struct snowflake_node {
+    int snowflake[SNOWFLAKE_SIDES];
+    struct snowflake_node *next;
+} snowflake_node;
+
+int code(int snowflake[]) {
     int sum = 0;
     for (int i = 0; i < SNOWFLAKE_SIDES; i++)
         sum += snowflake[i];
@@ -39,7 +45,7 @@ int are_identical(int snow1[], int snow2[]) {
     return 0;
 }
 
-void identify_identical(int snowflakes[][SNOWFLAKE_SIDES], int n) {
+void identify_identical_original(int snowflakes[][SNOWFLAKE_SIDES], int n) {
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (are_identical(snowflakes[i], snowflakes[j])) {
@@ -52,21 +58,49 @@ void identify_identical(int snowflakes[][SNOWFLAKE_SIDES], int n) {
     return;
 }
 
+void identify_identical(snowflake_node *snowflakes[]) {
+    snowflake_node *node1, *node2;
+    
+    for (int i = 0; i < SIZE; i++) {
+        node1 = snowflakes[i];
+        while (node1 != NULL) {
+            node2 = node1->next;
+            while (node2 != NULL) {
+                if (are_identical(node1->snowflake, node2->snowflake)) {
+                    printf("Twin snowflakes found.\n");
+                    return;
+                }
+                node2 = node2->next;
+            }
+            node1 = node1->next;
+        }
+    }
+    printf("No two snowflakes are alike.\n");
+}
+
 int main(void) {
 
-    static int snowflakes[SIZE][6];
-    int n;
+    static snowflake_node *snowflakes[SIZE] = {NULL};
+    snowflake_node *snow;
+    int n, snowflake_code;
     if (scanf("%d", &n) != 1) {
         fprintf(stderr, "invalid input\n");
         return 1;
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
+        snow = malloc(sizeof(snowflake_node));
+        if (snow == NULL) {
+            fprintf(stderr, "malloc error\n");
+            return 1;
+        }
         for (int j = 0; j < SNOWFLAKE_SIDES; j++)
-            if (scanf("%d", &snowflakes[i][j]) != 1) {
-                fprintf(stderr, "invalid input\n");
-                return 1;
-            }
-    identify_identical(snowflakes, n);
+            scanf("%d", &snow->snowflake[j]);
+        snowflake_code = code(snow->snowflake);
+        snow->next = snowflakes[snowflake_code];
+        snowflakes[snowflake_code] = snow;
+    }
+
+    identify_identical(snowflakes);
 
     return 0;
 }
