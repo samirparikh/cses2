a58cc766a96777125cc6a22ededd6ab5df2b8fa6
Samir Parikh <siparikh@gmail.com>
2026-07-14 20:44:05 -0400

    solve sample problem a plus b

---
+++ dmoj/a_plus_b/a_plus_b.c
@@ -0,0 +1,14 @@
+// dmoj: A Plus B
+
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d\n", &N);
+
+    for (int i = 0; i < N; i++) {
+        int a, b;
+        scanf("%d %d\n", &a, &b);
+        printf("%d\n", a + b);
+    }
+}
