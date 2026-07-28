06bdaf3cdc95e082f89aa7eb1835a329347c8471
Samir Parikh <siparikh@gmail.com>
2026-07-28 19:56:00 -0400

    find initial solution that is too slow

--- dmoj/coci17c1p3hard/coci17c1p3hard.c
+++ dmoj/coci17c1p3hard/coci17c1p3hard.c
@@ -1,8 +1,44 @@
 // dmoj: coci17c1p3hard
 
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_USERS 100000
+#define MAX_PASSWORD 10
 
 int main(void) {
+
+    static char users[MAX_USERS][MAX_PASSWORD + 1];
+    int num_ops, op, op_type, total, j;
+    char password[MAX_PASSWORD + 1];
+    int num_users = 0;
+
+    if (scanf("%d", &num_ops) != 1) {
+        fprintf(stderr, "invalid input\n");
+        return 1;
+    }
+
+    for (op = 0; op < num_ops; op++) {
+        if (scanf("%d%s", &op_type, password) != 2) {
+            fprintf(stderr, "invalid input\n");
+            return 1;
+        }
+
+        // add new user password to users[]
+        if (op_type == 1) {
+            strcpy(users[num_users], password);
+            num_users++;
+        }
+
+        // query user input against all passwords in user[]
+        else {
+            total = 0;
+            for (j = 0; j < num_users; j++)
+                if (strstr(users[j], password))
+                    total++;
+            printf("%d\n", total);
+        }
+    }
 
     return 0;
 }
