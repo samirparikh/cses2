e20133a324b268b2ab03a2024a90e595f5c67d36
Samir Parikh <siparikh@gmail.com>
2026-08-20 22:10:05 -0400

    solve bit strings

--- introductory_problems/bit_strings/bit_strings.c
+++ introductory_problems/bit_strings/bit_strings.c
@@ -2,7 +2,22 @@
 
 #include <stdio.h>
 
+#define MODULUS 1000000007
+
 int main(void) {
+
+    unsigned long long n;
+    unsigned long long result = 1;
+    if (scanf("%llu", &n) != 1) {
+        fprintf(stderr, "invalid input\n");
+        return 1;
+    }
+
+    for (unsigned long i = 1; i <= n; i++) {
+        result = (result * 2) % MODULUS;
+    }
+
+    printf("%llu\n", result);
 
     return 0;
 }
